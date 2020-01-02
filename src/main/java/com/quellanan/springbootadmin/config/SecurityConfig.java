da96d490dd33cb2ed60647f2f524053d2df47635
QuellanAn <1186154608@qq.com>
2020-01-02 20:20:46 +0800

    Initial commit

---
+++ src/main/java/com/quellanan/springbootadmin/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.quellanan.springbootadmin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+/**
+ * @ClassName SecurityConfig
+ * @Description DOTO
+ * @Author zhulinfeng
+ * @Date 2020/1/2 13:34
+ * @Version 1.0
+ */
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler
+                = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl("/");
+
+        http.authorizeRequests()
+                .antMatchers("/assets/**").permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin().loginPage("/login")
+                .successHandler(successHandler).and()
+                .logout().logoutUrl("/logout").and()
+                .httpBasic().and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers(
+                        "/instances",
+                        "/actuator/**"
+                );
+    }
+}
